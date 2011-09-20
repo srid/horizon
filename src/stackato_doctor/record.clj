d47ec976eb8e785b4400b7111637dd49d9edfca5
Sridhar Ratnakumar <sridharr@activestate.com>
2011-09-20 16:10:18 -0700

    refactor code in respective namespaces

---
+++ src/stackato_doctor/record.clj
@@ -0,0 +1,70 @@
+(ns stackato-doctor.record
+  (:use [clojure.contrib.duck-streams :only (read-lines)]
+        [clojure.contrib.json :only (read-json)]
+        [clojure.contrib.cond :only (cond-let)]
+        [clj-time.core :only (date-time)]
+        [clj-time.format :only (unparse formatter)]
+        [clansi]))
+
+(defn parse-log-datetime
+  "Parse datetime format of CF logging"
+  [time]
+  (if time
+    (apply date-time
+           (->> time
+                (re-matches #"\[(\d+)\-(\d+)\-(\d+) (\d+)\:(\d+)\:(\d+)\]")
+                next
+                (map #(Integer/parseInt %))))))
+
+(def ^{:private true} log-record-types
+  {;; Beginning of DEA app start
+   :received-start-message
+   [#"^DEA received start message\: (.+)$"
+    (fn [[json-data]]
+      (let [j (read-json json-data)]
+        {:json j, :users (:users j), :appname (:name j)}))]
+
+   ;; Earlier know time of container ID
+   :grabbed-container
+   [#"^Grabbed container dir .+ for instance (\w+)$"
+    [:container]],
+
+   ;; App is now ready for connections
+   :instance-ready-for-connections
+   [#"^Instance \(name=(\w+).+instance=(\w+).+is ready for connections, notifying system of status$"
+   [:appname :container]],
+   })
+
+(defn- match-record-type
+  "Match the given record to one of defined types and return its extra metadata."
+  [record]
+  (loop [[[key [pattern meta]] & more :as coll] (seq log-record-types)]
+    (when-not (empty? coll)
+      (if-let [groups (next (re-find pattern (:message record "")))]
+        (merge {:type key} (if (coll? meta) (zipmap meta groups) (meta groups)))
+        (recur more)))))
+  
+(defn parse-log-record
+  "Parse a Cf log record. Return a map."
+  [line]
+  (let [pattern #"(\[.+\]) ([^\s]+) \- (\w+ \w+ \w+)\s+(\w+) \-\- (.+)"
+        keys    [:when :component :identifier :level :message]
+        record  (zipmap keys (next (re-matches pattern line)))
+        record  (assoc record :type nil :when (parse-log-datetime (:when record)))]
+    (merge record (match-record-type record))))
+
+(defn print-log-record
+  "Print the log-record to terminal"
+  [record host]
+  (let [record1         (dissoc record :type :message :foo :json :level :component :when)
+        time-to-display (unparse (formatter "MM-dd/hh:mm:ss") (:when record))]
+    (println (format "[%.15s] %s %s -- %s"
+                     host
+                     (style time-to-display :underline)
+                     (style (:type record) :green)
+                     (if (nil? (:type record)) (:message record) record1)))))
+
+(defn events-timeline
+  "Return special log records"
+  [records]
+  (filter #(not (nil? (:type %))) records))
--- src/stackato_doctor/sink.clj
+++ src/stackato_doctor/sink.clj
@@ -1,4 +1,4 @@
-(ns stackato-doctor.logmerge
+(ns stackato-doctor.sink
   (use [clojure.contrib.shell-out :only (sh)]
        [clojure.contrib.string :only (split-lines)]
        [lamina.core :as lm]))
@@ -15,21 +15,20 @@
        clojure.java.io/reader
        line-seq))
 
-(defn dea-tail-f
+(defn- dea-tail-f
   "Returns the dea log lines (lazy seq) from this host"
   [host]
   (sh-line-seq (str "scripts/tail-dea-log " host)))
 
 ;; log queue
-(def log-queue (lm/channel))
+(def ^{:private true} log-queue (lm/channel))
 (defn- register-log-source [host lazy-lines]
   (future
     (doseq [line lazy-lines]
       (lm/enqueue log-queue [host line]))))
 
-
 ;; mapping from hostname to open line-seq on dea.log (via ssh/tail-f)
-(def running-deas (agent {}))
+(def ^{:private true} running-deas (agent {}))
 (defn- update-running-deas
   "Update the running-deas agent with new set of deas from AWS"
   [curr]
@@ -44,12 +43,12 @@
           (println "Added new DEA: " host)
           (recur (next newhosts)
                  (assoc nxt host tailer)))))))
+
 (defn- remove-running-dea
   [host]
   (future-cancel (@running-deas host))
   (println "Removed DEA: " host " " (future-cancelled? (@running-deas host)))
   (send running-deas dissoc host))
-
 
 (defn initialize
   "Initialize log processing from EC2"
@@ -70,12 +69,11 @@
   []
   (lm/wait-for-message log-queue))
 
-
 (defn -main []
   (initialize)
   (loop []
     (let [[src log] (next-log-record)]
       (println (format "%.15s:    %s" src log)))
     ; (println (format "%d log entries; from %d dea's" (count log-queue) (count @running-deas)))
-    ;; (send running-deas update-running-deas)
+    ;; TODO - (send running-deas update-running-deas)
     (recur)))
