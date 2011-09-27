72150f6158f79352f75ae7459d93018c222cd9be
Sridhar Ratnakumar <sridharr@activestate.com>
2011-09-27 13:07:55 -0700

    really trivial clojureql fetching of apps

---
+++ src/stackato_dashboard/db.clj
@@ -0,0 +1,12 @@
+(ns stackato-dashboard.db
+  (:require [clojureql.core :as cql]
+            [clojure.contrib.sql :as sql]))
+
+(def db {:classname "org.sqlite.JDBC"
+         :subprotocol "sqlite"
+         :subname "/Users/sridharr/cloudcontroller.sqlite3"})
+
+(defn play []
+  (sql/with-connection db
+    (cql/with-results [rs (cql/table :apps)]
+      (doseq [r rs] (println (:name r))))))
