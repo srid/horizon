3030c136a9728aad4e18d2f9da046af9947c098f
Sridhar Ratnakumar <sridharr@activestate.com>
2011-09-28 16:29:56 -0700

    Go back to clj 1.2 for clj-record

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject stackato-dashboard "1.0.0-SNAPSHOT"
   :description "Stackato dashboard"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/tools.logging "0.2.0"]
                  [org.clojure/data.json "0.1.1"]
                  [org.clojure/java.jdbc "0.0.6"]
@@ -8,13 +8,10 @@
                  [org.xerial/sqlite-jdbc "3.7.2"]
                  [clj-time "0.3.0"]
                  [clansi "1.0.0"]
-                 ;; duck1123 maintains a working fork with Clojure 1.3 support
-                 [net.kronkltd/lamina "0.4.0-beta2-SNAPSHOT"]
-                 [net.kronkltd/aleph "0.2.0-beta2-SNAPSHOT"]
+                 [clj-record "1.0-SNAPSHOT"]
+                 [lamina "0.4.0-beta2"]
+                 [aleph "0.2.0-beta2"]
                  [compojure "0.6.4"]
                  [enlive "1.0.0"]
                  [ring "0.3.11"]]
-  :repositories {"jiksnu-snapshots" "http://build.jiksnu.com/repository/snapshots"}
-  :exclusions [org.clojure/contrib
-               org.clojure/clojure-contrib]
   :main stackato-dashboard.core)
