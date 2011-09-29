057f6786bedf0982cef768c0ce3d51254379fb57
Sridhar Ratnakumar <sridharr@activestate.com>
2011-09-29 11:58:48 -0700

    Use clojureql for reading from sqlite

--- src/stackato_dashboard/db.clj
+++ src/stackato_dashboard/db.clj
@@ -1,11 +1,13 @@
 (ns stackato-dashboard.db
-  (:use [clojure.java.jdbc :only (with-connection with-query-results)]))
+  (:use [clojure.java.jdbc :only (with-connection with-query-results)])
+  (:require [clojureql.core :as q]))
 
 (def db {:classname "org.sqlite.JDBC"
          :subprotocol "sqlite"
          :subname "/Users/sridharr/cloudcontroller.sqlite3"})
 
+(q/open-global db)
+
 (defn play []
-  (with-connection db
-    (with-query-results rs ["select * from apps"]
-      (doseq [r rs] (println (:name r))))))
+  [@(q/table :apps) @(q/table :users) @(q/table :service_bindings)])
+
