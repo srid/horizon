9a901aee67c093ce7691ff90bf35ef7299d851d7
Sridhar Ratnakumar <sridharr@activestate.com>
2011-10-27 10:29:23 -0700

    Remove the no-longer-required title attribute (debug)

--- src/clj/horizon/web.clj
+++ src/clj/horizon/web.clj
@@ -27,7 +27,7 @@
     [:b [:a {:href url :target "_blank"} (:appname record)]]))
 
 (defn- record-html [record]
-  [:div {:title (str record) :class (clojure.string/join " " ["event_record" (:event_type record)])}
+  [:div {:class (str "event_record " (:event_type record))}
    [:b (record/format-log-datetime record)] " -- "
    (condp = (:event_type record)
      "dea_ready"
