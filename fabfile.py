cfc8f7b4ef1938cec7618ca446ca2178fe92a470
Sridhar Ratnakumar <sridharr@activestate.com>
2011-11-03 11:01:43 -0700

    fab: after staging deployment, check if the site is up

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,8 @@
+from time import sleep
 from glob import glob
+from urllib2 import urlopen, URLError
 from fabric.api import run, local, env, put
-
+from fabric.colors import *
 env.hosts = ['stackato@stackato.activestate.com']
 
 def _uberjar():
@@ -8,7 +10,28 @@ def _uberjar():
     local('lein uberjar')
 
 def deploy_staging():
-    _uberjar()
+    "Deploy dashboard to stackato.activestate.com"
+    #_uberjar()
     run('rm -f stackato/horizon/*.jar')
+    run('stackato-admin stop dashboard')
+    run('rm -f /tmp/vcap-run/horizon.log')
     put(glob('horizon-*-standalone.jar')[0], 'stackato/horizon/')
     run('stackato-admin start dashboard')
+    check_staging()
+    
+
+def check_staging():
+    url = "http://stackato.activestate.com:7654/"
+    print("Checking %s" % url)
+    while True:
+        try:
+            f = urlopen(url)
+        except URLError, e:
+            print(red('FAIL - %s' % e))
+        else:
+            if f.code == 200:
+                print(green('OK'))
+            else:
+                print(red('Server returned unexpected HTTP code: %s' % f.code))
+            break
+        sleep(2)
